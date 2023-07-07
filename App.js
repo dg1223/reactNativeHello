82d916a8f8416f882cf0d71f64141fb77a2bfcba
dg1223 <alavi1223@hotmail.com>
2023-07-07 22:44:46 +0600

    feat: added branch name

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 // import { StatusBar } from "expo-status-bar";
-import React from "react";
+// import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 const App = () => {
   return (
     <View style={styles.container} testID="container">
       <Text>Hello World</Text>
+      <Text>On branch: main</Text>
       {/* <StatusBar style="auto" /> */}
     </View>
   );
