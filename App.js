7327787ff31b4129b72c0696f123a0443df774e3
dg1223 <alavi1223@hotmail.com>
2023-07-08 18:44:28 +0600

    feat: updated On branch to From branch

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <View style={styles.container} testID="container">
       <Text>Hello World</Text>
-      <Text>On branch: main</Text>
+      <Text>From branch: main</Text>
       {/* <StatusBar style="auto" /> */}
     </View>
   );
