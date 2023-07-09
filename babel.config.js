caecf53082703fa5b2983576b3ff8214aff6f249
dg1223 <alavi1223@hotmail.com>
2023-07-09 18:00:36 +0600

    fix: removed preset from babel.config.js that was causing android build issues

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "@babel/preset-env", "@babel/preset-react"],
+    presets: ["babel-preset-expo", "@babel/preset-react"],
     plugins: [
       ["@babel/plugin-transform-private-methods", { loose: true }],
       ["@babel/plugin-transform-private-property-in-object", { loose: true }],
